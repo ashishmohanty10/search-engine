0c1890178e2e7cb7987077a4d9d4ba2914944bc7
ASHISH MOHANTY <ashishmohaty10@gmail.com>
2024-06-19 23:22:10 +0530

    almost done

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -4,13 +4,16 @@ import { Loader2, SearchIcon } from "lucide-react";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 import { useRef, useState, useTransition } from "react";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 
 const SearchBar = () => {
+  const searchParams = useSearchParams();
+  const defaultQuery = searchParams.get("query") || "";
+
   const inputRef = useRef<HTMLInputElement>(null);
   const [isSearching, startTransition] = useTransition();
   const router = useRouter();
-  const [query, setQuery] = useState<string>("");
+  const [query, setQuery] = useState<string>(defaultQuery);
 
   const search = () => {
     startTransition(() => {
